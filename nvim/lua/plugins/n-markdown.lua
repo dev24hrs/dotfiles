b81334b3776ebe6cc5f796306938cebf9e18fc0a
dev24hrs <asang24@foxmail.com>
2024-08-13 10:33:05 +0800

    update markview.nvim

--- nvim/lua/plugins/n-markdown.lua
+++ nvim/lua/plugins/n-markdown.lua
@@ -1,11 +1,30 @@
 return {
-  {
-    'iamcco/markdown-preview.nvim',
-    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
-    ft = { 'markdown' },
-    build = function()
-      vim.fn['mkdp#util#install']()
-    end,
-    vim.keymap.set('n', '<leader>mp', '<cmd>MarkdownPreview<CR>', { desc = '[M]arkdown [P]review' }),
+  'OXY2DEV/markview.nvim',
+  lazy = false, -- Recommended
+  -- ft = "markdown" -- If you decide to lazy-load anyway
+
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter',
+    'nvim-tree/nvim-web-devicons',
   },
+  config = function()
+    local markview = require('markview')
+    local presets = require('markview.presets')
+
+    markview.setup({
+      headings = presets.headings.glow_labels,
+
+      modes = { 'n', 'i', 'no', 'c' },
+      hybrid_modes = { 'i' },
+
+      -- This is nice to have
+      callbacks = {
+        on_enable = function(_, win)
+          vim.wo[win].conceallevel = 2
+          vim.wo[win].concealcursor = 'nc'
+        end,
+      },
+    })
+    vim.cmd('Markview enableAll')
+  end,
 }
