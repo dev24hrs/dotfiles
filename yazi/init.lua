ceef49adfeed3caf0843bd3d1b8d69a747e7da25
dev24hrs <asang24@foxmail.com>
2025-06-27 17:15:43 +0800

    custom yazi git status sign

--- yazi/init.lua
+++ yazi/init.lua
@@ -1,10 +1,11 @@
-require("git"):setup()
-
+-- this custom config should by writen before git setup.
 th.git = th.git or {}
-th.git.added_sign = "A"
 th.git.modified_sign = "M"
 th.git.deleted_sign = "D"
-th.git.untracked_sign = "?"
+th.git.added_sign = "A"
 th.git.updated_sign = "U"
+th.git.ignored_sign = "I"
+th.git.untracked_sign = "?"
 
-require("full-border"):setup()
+require("git"):setup()
+require("full-border"):setup({})
