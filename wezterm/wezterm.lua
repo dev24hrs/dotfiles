a8cc18e08098da33a67f3a012c2609ae3711699b
dev24hrs <asang24@foxmail.com>
2025-10-23 21:54:43 +0800

    update wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -9,7 +9,7 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices.
 
-config.font = wezterm.font('RecMonoCasual Nerd Font')
+config.font = wezterm.font('RecMonoCasual Nerd Font Mono')
 config.font_size = 16
 
 config.enable_tab_bar = false 
@@ -19,7 +19,7 @@ config.window_decorations = 'RESIZE'
 config.window_background_opacity = 0.95
 config.macos_window_background_blur = 30
 
-config.initial_cols = 150
+config.initial_cols = 140
 config.initial_rows = 45 
 
 config.window_close_confirmation = 'NeverPrompt'
