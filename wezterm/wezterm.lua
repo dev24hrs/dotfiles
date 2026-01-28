73ceb63185655a88f2b2d91faa77c96235d961eb
dev24hrs <asang24@foxmail.com>
2026-01-28 13:36:48 +0800

    feat: update wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,7 +1,4 @@
 --  refer to https://wezterm.org/config/files.html
--- https://wezterm.org/colorschemes/e/index.html#everforest-dark-medium-gogh
-
--- refer to https://github.com/wsgggws/slides-md-ppt/blob/main/wezterm/wezterm.lua
 
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
@@ -22,9 +19,8 @@ config.default_cursor_style = "SteadyBar"
 
 config.window_close_confirmation = "NeverPrompt"
 
-config.color_scheme = "Gruvbox Dark (Gogh)"
-
--- config.color_scheme = 'Gruvbox dark, medium (base16)'
+-- config.color_scheme = "Gruvbox Dark (Gogh)"
+config.color_scheme = "Gruvbox dark, medium (base16)"
 
 config.adjust_window_size_when_changing_font_size = false
 
