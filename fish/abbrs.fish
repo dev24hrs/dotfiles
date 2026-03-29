2a9f44fcede1f9f41d99b4efe5ec632bf675435a
dev24hrs <asang24dev@foxmail.com>
2026-03-29 23:27:18 +0800

    feat: update abbr for git

--- fish/abbrs.fish
+++ fish/abbrs.fish
@@ -2,10 +2,31 @@
 # gs gb gl rewrite by git.fish
 # abbr -a gs 'git status'
 abbr -a ga 'git add'
-abbr -a gc 'git commit -m'
-abbr -a gp 'git pull'
-abbr -a gd 'git diff'
+abbr -a gaa 'git add --all'
+abbr -a gcm 'git commit -m'
+abbr -a gca 'git commit --amend'
+
 abbr -a gco 'git checkout'
+abbr -a gsw 'git switch'
+abbr -a gsc 'git switch -c'
+
+abbr -a gd 'git diff'
+abbr -a gf 'git fetch'
+abbr -a gl 'git pull'
+abbr -a gp 'git push'
+abbr -a gpf 'git push --force-with-lease' # 比 --force 更安全
+
+abbr -a gst 'git stash'
+abbr -a gsp 'git stash pop'
+abbr -a gsl 'git stash list'
+
+abbr -a gr 'git rebase'
+abbr -a gra 'git rebase --abort'
+abbr -a grc 'git rebase --continue'
+abbr -a gri 'git rebase -i'
+abbr -a grh 'git reset --hard'
+abbr -a grs 'git reset --soft'
+
 # abbr -a gl "git log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cd) %C(bold blue)<%an>%Creset' --abbrev-commit -n 20 --date=format:'%Y-%m-%d %H:%M %a'"
 
 # tmux
