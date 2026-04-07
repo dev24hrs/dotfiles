cd05460010da05dca7cde8ae0037ccd7ddc1e05c
dev24hrs <asang24dev@gmail.com>
2026-04-07 16:40:04 +0800

    feat: add brew/bin path

--- fish/conf.d/envPath.fish
+++ fish/conf.d/envPath.fish
@@ -9,13 +9,14 @@ set -gx MAVEN_HOME $HOME/Documents/Tools/maven/maven-3.9.9
 set -gx GRADLE_HOME $HOME/Documents/Tools/gradle/gradle-8.12
 set -gx GRADLE_USER_HOME $HOME/Documents/Tools/gradle/gradle-repository
 
+set -gx PATH /opt/homebrew/bin $PATH
+
 # 3. 路径管理
 # $GOPATH/bin: go install xxx 的存放路径
 # /opt/homebrew/opt/go: 是 Homebrew 指向当前 Go 版本的软链
 # 可用于多个 Go 版本时指定某一个版本
 
 fish_add_path $GOPATH/bin
-fish_add_path /opt/homebrew/opt/go/bin
 fish_add_path $JAVA_HOME/bin
 fish_add_path $MAVEN_HOME/bin
 fish_add_path $GRADLE_HOME/bin
